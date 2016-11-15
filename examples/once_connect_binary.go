d5875bf7da958f5375fd955bf862ba8f67277288
weiwufeng <weiwufeng@sohu.com>
2016-11-15 12:13:37 +0800

    ...

---
+++ examples/once_connect_binary.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/blackspace/gomail"
+	"log"
+	"github.com/blackspace/gomail/mailclient"
+	"sync"
+	"net"
+	"time"
+	"github.com/blackspace/gomail/mailbinaryserver"
+)
+
+var started = make(chan int)
+
+const TIME = 1 << 10
+
+func _Server() {
+	mailbinaryserver.Start()
+	defer mailbinaryserver.Stop()
+	c := 0
+
+	started <- 1
+	start := time.Now()
+
+	for i := 0; i < TIME; i++ {
+		gomail.MailBox.GetMail()
+		c++
+	}
+
+	log.Println(c)
+	log.Println(time.Now().Sub(start))
+
+}
+
+func _Client() {
+	<-started
+	conn, err := net.Dial("tcp", "127.0.0.1:5050")
+	defer func() {
+		conn.Close()
+	}()
+	if err == nil {
+		for i := 0; i < TIME; i++ {
+			mailclient.SendMailBinary("127.0.0.1:5050", []byte("hello"))
+		}
+	} else {
+		log.Println(err)
+	}
+}
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		_Server()
+		wg.Done()
+	}()
+
+	go func() {
+		_Client()
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
